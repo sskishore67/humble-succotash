a4a1f19bd5d62d4b36211e7766b8eba86a938085
Kishore S <76157627+sskishore67@users.noreply.github.com>
2024-02-20 13:58:25 +0000

    Pending changes exported from your codespace

---
+++ test1.py
@@ -0,0 +1,89 @@
+import os
+import cv2
+import numpy as np
+import keras
+
+import segmentation_models as sm
+from Loader import *
+
+train_x = "VesselSegmentationImages/img"
+train_y = "VesselSegmentationImages/mask"
+
+valid_x = "VesselSegmentationImages/vaild/img"
+valid_y = "VesselSegmentationImages/vaild/mask"
+
+
+dataset = Dataset(train_x, train_y, classes=['retinal', 'blood_vessel'],augmentation=get_training_augmentation())
+
+BACKBONE = 'efficientnetb3'
+BATCH_SIZE = 8
+CLASSES = ['retinal']
+LR = 0.0001
+EPOCHS = 40
+
+preprocess_input = sm.get_preprocessing(BACKBONE)
+
+# define network parameters
+n_classes = 1 if len(CLASSES) == 1 else (len(CLASSES) + 1)  # case for binary and multiclass segmentation
+activation = 'sigmoid' if n_classes == 1 else 'softmax'
+
+#create model
+model = sm.Unet(BACKBONE, classes=n_classes, activation=activation)
+
+# define optomizer
+optim = keras.optimizers.Adam(LR)
+
+# Segmentation models losses can be combined together by '+' and scaled by integer or float factor
+dice_loss = sm.losses.DiceLoss()
+focal_loss = sm.losses.BinaryFocalLoss() if n_classes == 1 else sm.losses.CategoricalFocalLoss()
+total_loss = dice_loss + (1 * focal_loss)
+
+# actulally total_loss can be imported directly from library, above example just show you how to manipulate with losses
+# total_loss = sm.losses.binary_focal_dice_loss # or sm.losses.categorical_focal_dice_loss 
+
+metrics = [sm.metrics.IOUScore(threshold=0.5), sm.metrics.FScore(threshold=0.5)]
+
+# compile keras model with defined optimozer, loss and metrics
+model.compile(optim, total_loss, metrics)
+
+# Dataset for train images
+train_dataset = Dataset(
+    train_x, 
+    train_y, 
+    classes=CLASSES, 
+    augmentation=get_training_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+# Dataset for validation images
+valid_dataset = Dataset(
+    valid_x, 
+    valid_y, 
+    classes=CLASSES, 
+    augmentation=get_validation_augmentation(),
+    preprocessing=get_preprocessing(preprocess_input),
+)
+
+train_dataloader = Dataloder(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+valid_dataloader = Dataloder(valid_dataset, batch_size=1, shuffle=False)
+
+# check shapes for errors
+print(train_dataloader[0][0].shape)  # Add this line to print the shape
+assert train_dataloader[0][0].shape == (BATCH_SIZE, 512, 512, 3)
+assert train_dataloader[0][1].shape == (BATCH_SIZE, 512, 512, n_classes)
+
+# define callbacks for learning rate scheduling and best checkpoints saving
+callbacks = [
+    keras.callbacks.ModelCheckpoint('best_model.h5', save_weights_only=True, save_best_only=True, mode='min'),
+    keras.callbacks.ReduceLROnPlateau(),
+]
+
+# train model
+history = model.fit(
+    train_dataloader, 
+    steps_per_epoch=len(train_dataloader), 
+    epochs=EPOCHS, 
+    callbacks=callbacks, 
+    validation_data=valid_dataloader, 
+    validation_steps=len(valid_dataloader),
+)
